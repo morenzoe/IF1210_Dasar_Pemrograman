f687abb1deb5f2f66f7c5b478604ffbf5e21a6f0
Eraraya Morenzo <morenzomuten@gmail.com>
2021-02-19 03:50:16 +0700

    Exercises update.

---
+++ haskell-programs/exercises/week-4-list/konstruktor.hs
@@ -0,0 +1,16 @@
+--DEFINISI DAN SPESIFIKASI KONSTRUKTOR
+konso:: <type_elemen>-> [<type_elemen>] -> [<type_elemen>]
+{-  konso(e,l) menghasilkan sebuah list dari e (sebuah--elemen) 
+    dan l (list of elemen), dengan e sebagai elemen pertama: 
+    e o l -> l' -}
+
+--REALISASI
+konsoel = [e] ++ l
+
+konsDot:: [<type_elemen>]-> <type_elemen>-> [<type_elemen>]
+{-  konsDot(l,e) menghasilkan sebuah list dari l (list of
+    elemen) dan e (sebuah elemen), dengan e sebagai elemen terakhir: 
+    l •e -> l' -}
+
+--REALISASI
+konsDotle = l ++ [e]
---
+++ haskell-programs/template.hs
@@ -0,0 +1,10 @@
+-- JUDUL                    namafungsi(p,a,r,a,m)
+
+-- DEFINISI DAN SPESIFIKASI
+namafungsi :: Type -> Type -> Type
+{-  
+     -}
+
+-- REALISASI
+
+-- APLIKASI
