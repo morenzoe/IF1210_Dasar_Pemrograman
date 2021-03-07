a399b93155c52c721fa47610ed7b949984ffc04f
Eraraya Morenzo <morenzomuten@gmail.com>
2021-03-07 08:45:22 +0700

    Lab update.

---
+++ haskell-programs/practicum/week-2-recursive-list/alternateSort.hs
@@ -0,0 +1,119 @@
+module AlternateSort where
+-- DEFINISI DAN SPESIFIKASI LIST
+{- type List of Int: [ ] atau [e o List] atau [List o e]  
+   Definisi type List of Int
+   Basis: List of Int kosong adalah list of Int 
+   Rekurens: 
+   List tidak kosong dibuat dengan menambahkan sebuah elemen bertype Int di awal 
+   sebuah list atau
+   dibuat dengan menambahkan sebuah elemen bertype Int di akhir sebuah list -}
+
+-- DEFINISI DAN SPESIFIKASI KONSTRUKTOR
+konso :: Int -> [Int] -> [Int]
+{- konso e li menghasilkan sebuah list of integer dari e (sebuah integer) dan li 
+   (list of integer), dengan e sebagai elemen pertama: e o li -> li' -}
+-- REALISASI
+konso e li = [e] ++ li
+
+konsDot :: [Int] -> Int -> [Int]
+{- konsDot li e menghasilkan sebuah list of integer dari li (list of integer) dan 
+   e (sebuah integer), dengan e sebagai elemen terakhir: li o e -> li' -}
+-- REALISASI
+konsDot li e = li ++ [e]
+
+-- DEFINISI DAN SPESIFIKASI SELEKTOR
+-- head :: [Int] -> Int
+-- head l menghasilkan elemen pertama list l, l tidak kosong
+
+-- tail :: [Int] -> [Int]
+-- tail l menghasilkan list tanpa elemen pertama list l, l tidak kosong
+
+-- last :: [Int] -> Int
+-- last l menghasilkan elemen terakhir list l, l tidak kosong
+
+-- init :: [Int] -> [Int]
+-- init l menghasilkan list tanpa elemen terakhir list l, l tidak kosong
+
+-- DEFINISI DAN SPESIFIKASI PREDIKAT
+isEmpty :: [Int] -> Bool
+-- isEmpty l  true jika list of integer l kosong
+-- REALISASI
+isEmpty l = null l
+
+isOneElmt :: [Int] -> Bool
+-- isOneElmt l true jika list of integer l hanya mempunyai satu elemen
+-- REALISASI
+isOneElmt l = (length l) == 1 
+
+-- DEFINISI DAN SPESIFIKASI FUNGSI ANTARA
+delElmt :: Int -> [Int] -> [Int]
+{- delElmt n li akan mengembalikan list of integer li tanpa
+   elemen integer n. -}
+
+getSmallest :: [Int] -> Int
+{- getSmallest li akan mengembalikan elemen integer terkecil dari
+   suatu list of integer li. 
+   Prekondisi: li tidak kosong. -}
+
+sortList :: [Int] -> [Int]
+{- sortList li akan mengembalikan li yang terurut tidak mengecil. -}
+
+splitList :: [Int] -> ([Int], [Int])
+{- splitList li akan membagi list of integer li menjadi dua list of integer 
+   dengan jumlah elemen yang sama. Jika jumlah elemen li ganjil, list of 
+   integer pertama akan 1 elemen lebih banyak. -}
+
+getHeadl1 :: ([Int],[Int]) -> [Int]
+{- getHead (l1,l2) akan mengambil elemen pertama l1. -}
+
+getLastl2 :: ([Int],[Int]) -> [Int]
+{- getLast (l1,l2) akan mengambil elemen terakhir l2. -}
+
+-- REALISASI FUNGSI ANTARA
+getSmallest li
+    | isOneElmt li = head li -- basis
+    | otherwise = -- rekurens
+        let
+            n = getSmallest (tail li) 
+        in
+            if ((head li) < n) then head li
+            else n
+
+delElmt n li
+ | isEmpty li = li -- basis list kosong
+ | ((head li) == n) = tail li -- basis n adalah elemen pertama li
+ | otherwise = konso (head li) (delElmt n (tail li)) -- rekurens
+
+sortList li
+ | isEmpty li = [] -- basis
+ | otherwise =  -- rekurens
+        let
+            n = getSmallest li
+        in
+            konso n (sortList (delElmt n li))
+
+splitList l
+    | isEmpty l = ([], []) -- basis kedua list kosong
+    | isOneElmt l = (l, []) -- basis list terdiri dari satu elemen
+    | otherwise = -- rekurens
+        let
+            (lDepan, lBelakang) = splitList (tail (init l))
+        in
+            (konso (head l) lDepan, konsDot lBelakang (last l))
+
+getHeadl1 (l1,l2)
+ | isEmpty l1 = [] -- basis
+ | otherwise = konso (head l1) (getLastl2 (tail l1,l2)) -- rekurens
+
+getLastl2 (l1,l2)
+ | isEmpty l2 = [] -- basis
+ | otherwise = konso (last l2) (getHeadl1 (l1,init l2)) -- rekurens
+
+-- DEFINISI DAN SPESIFIKASI 
+alternateSort :: [Int] -> [Int]
+
+-- REALISASI
+alternateSort li = getHeadl1 (splitList (sortList li))
+
+-- APLIKASI
+-- alternateSort [9,10,11,12]
---
+++ haskell-programs/practicum/week-2-recursive-list/makeUnique.hs
@@ -0,0 +1,74 @@
+module ListOfCharacter where
+-- DEFINISI DAN SPESIFIKASI LIST
+{- type List of Char: [ ] atau [e o List] atau [List o e]
+   Definisi type List of Char
+   Basis: List of Char kosong adalah list of Char
+   Rekurens: 
+   List tidak kosong dibuat dengan menambahkan sebuah elemen bertype Char di awal 
+   sebuah list atau
+   dibuat dengan menambahkan sebuah elemen bertype Char di akhir sebuah list -}
+
+-- DEFINISI DAN SPESIFIKASI KONSTRUKTOR
+konso :: Char -> [Char] -> [Char]
+{- konso e lc menghasilkan sebuah list of character dari e (sebuah character)  
+   dan lc (list of integer), dengan cc sebagai elemen pertama: e o lc -> lc' -}
+-- REALISASI
+konso e lc = [e] ++ lc
+
+konsDot :: [Char] -> Char -> [Char]
+{- konsDot(lc,cc) menghasilkan sebuah list of character dari lc (list of 
+   character) dan e (sebuah character), dengan e sebagai elemen terakhir: 
+   lc o e -> lc' -}
+-- REALISASI
+konsDot lc e = lc ++ [e]
+
+-- DEFINISI DAN SPESIFIKASI SELEKTOR
+-- head :: [Char] -> Char
+-- head l menghasilkan elemen pertama list l, l tidak kosong
+
+-- tail :: [Char] -> [Char]
+-- tail l menghasilkan list tanpa elemen pertama list l, l tidak kosong
+
+-- last :: [Char] -> Char
+-- last l  menghasilkan elemen terakhir list l, l tidak kosong
+
+-- init :: [Char] -> [Char]
+-- init(l) menghasilkan list tanpa elemen terakhir list l, l tidak kosong
+
+-- DEFINISI DAN SPESIFIKASI PREDIKAT
+isEmpty :: [Char] -> Bool
+-- isEmpty l true jika list of character l kosong
+-- REALISASI
+isEmpty l = null l
+
+isOneElmt :: [Char] -> Bool
+-- isOneElmt l true jika list of character l hanya mempunyai satu elemen
+-- REALISASI
+isOneElmt l = (length l) == 1 
+
+-- DEFNIISI DAN SPESIFIKASI FUNGSI ANTARA
+isMember :: Char -> [Char] -> Bool
+{- isMember1 x lc akan memberikan nilai True jika x adalah elemen 
+   dari sebuah list of character . -}
+
+--REALISASI FUNGSI ANTARA
+isMember x lc  
+ | (isEmpty lc) = False  -- basis kosong
+ | (head lc) == x = True -- basis kondisi terpenuhi
+ | otherwise = (isMember x (tail lc)) -- rekurens
+
+-- DEFINISI DAN SPESIFIKASI
+makeUnique :: [Char] -> [Char]
+{- makeUnique lc akan mengembalikan list of character yang berisi elemen unik
+   dari list of character input. Jika ada elemen yang sama, elemen yang
+   dipertahankan adalah elemen yang berada lebih di awal.
+   Prekondisi: elemen adalah character. -}
+
+-- REALISASI
+makeUnique lc
+ | isEmpty lc = [] -- basis
+ | otherwise = if (isMember (last lc) (init lc)) then makeUnique (init lc) -- rekurens
+		else konsDot (makeUnique (init lc)) (last lc)
+
+-- APLIKASI
+-- makeUnique ['k','k']
