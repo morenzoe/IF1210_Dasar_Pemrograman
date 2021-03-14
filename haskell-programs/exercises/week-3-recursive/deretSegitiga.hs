81752918256f21ba0ae1598b6a4565d5b5217eba
Eraraya Morenzo <morenzomuten@gmail.com>
2021-03-14 16:25:04 +0700

    Exercises update.

--- haskell-programs/exercises/week-3-recursive/deretSegitiga.hs
+++ haskell-programs/exercises/week-3-recursive/deretSegitiga.hs
@@ -3,7 +3,7 @@
 -- DEFINISI DAN SPESIFIKASI
 deretSegitiga :: Int -> Int
 {-  fungsi untuk mencari nilai bilangan ke-n pada deret segitiga
--}
+    yaitu 1, 3, 6, 10, 15, 21, 28, 36, ...-}
 
 -- REALISASI
 deretSegitiga n
